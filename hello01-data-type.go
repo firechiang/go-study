091846c1f6ae37e3bbb419181ed72c3eca5413c9
chiangfire <chiangfire@localhost.localdomain>
2022-05-27 16:04:29 +0800

    asa

--- hello01-data-type.go
+++ hello01-data-type.go
@@ -21,6 +21,7 @@ func main() {
 	variableType()
 	variableObject()
 	convertObject()
+	pointer()
 }
 
 /**
@@ -56,7 +57,16 @@ func variableObject() {
 	// 不指定类型直接定义变量
 	var a6, a7 = "12545", 10
 	var a4 string = "sfdasl"
-	// 使用冒号定义变量（这个和var等同意思）
+	// 使用冒号定义变量，表示初始赋值，不需要写var
 	a5 := 10
 	fmt.Printf("a1=%d,a2=%d,a3=%d,a4=%q,a5=%d,a6=%q,a7=%d", a1, a2, a3, a4, a5, a6, a7)
+}
+
+func pointer() {
+	var a int = 2
+	// pa指针指向a变量内存地址
+	var pa *int = &a
+	// 修改pa指针内存地址的值，也就是a变量的值
+	*pa = 3
+	fmt.Println(a)
 }
